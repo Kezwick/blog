f1810b0af2abdcc79dfdbc6364e4464cbb6b6a84
comp11-cab4 <comp11-cab4.ppk.lan>
2019-03-11 11:50:37 +1000

    commit add somebody this)

--- views/admin/edit-cat.php
+++ views/admin/edit-cat.php
@@ -11,28 +11,28 @@
 <hr />
 <div class="panel panel-default">
     <div class="panel-heading">
-        Давайте создадим новый шедевр!
+        {{random(['Давайте создадим новый шедевр!', 'О Да! Пиши в меня глубокого смысла тексты...', 'Великого флуда пост создай', 'Изречение величайшего ума вселенского масштаба'])}}
     </div>
     <div class="panel-body">
         <div class="row">
             <div class="col-md-12">
                 <h2 id="loading" style="text-align:center;">Now loading...</h2>
-                <form method="post" action="/admin/categoryedit">
+                <form method="post" action="/admin/cat-edit">
     <div class="form-group">
-        <label>Заголовок</label>
-        <input name="name" class="form-control" placeholder="Введите заголовок" />
+        <label>Название</label>
+        <input name="name" class="form-control" value="{{category.name}}" />
     </div>
 
-    <е о class="form-group">
+    <div class="form-group">
         <label>Описание</label>
-        <input name="name" class="form-control" placeholder="Введите описание" />
+        <input name="title" class="form-control"  value="{{category.description}}" />
     </div>
     <div class="form-group">
         <button type="submit" class="btn btn-default">Submit Button</button>
         <button type="reset" class="btn btn-primary">Reset Button</button>
 
     </div>
-</form>
+                </form>
             </div>
         </div>
     </div>
